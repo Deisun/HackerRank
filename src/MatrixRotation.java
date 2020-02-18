fd41b78f7eab8a657b3cf2be1d29147312a93e0c
Rob Douma <rdouma81@gmail.com>
2020-02-18 14:47:33 -0500

    Matrix Rotation practice

--- src/MatrixRotation.java
+++ src/MatrixRotation.java
@@ -6,6 +6,10 @@ public class MatrixRotation {
         int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
 
         printMatrix(matrix);
+
+        rotateMatrix(matrix, 1);
+
+        printMatrix(matrix);
     }
 
     private static void printMatrix(int [][] matrix) {
@@ -16,5 +20,30 @@ public class MatrixRotation {
             System.out.println();
         }
     }
+
+    private static void rotateMatrix(int [][] matrix, int times) {
+        while (times != 0) {
+            // transpose
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = i; j < matrix[i].length; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+
+            // swap
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < (matrix.length / 2); j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                    matrix[i][matrix.length - 1 - j] = temp;
+                }
+            }
+
+            times--;
+        }
+    }
+
 
 }
