51aaeb0e218a4c70a23313042004bf447e5d513f
Deisun <rdouma81@gmail.com>
2020-02-18 23:14:30 -0500

    First None Repeating Character

---
+++ src/FirstNoneRepeatingCharacter.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class FirstNoneRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        String s = "aaabbfffecc";
+
+        System.out.println(firstNonRepeatingCharacter(s));
+    }
+
+    public static char firstNonRepeatingCharacter(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (map.get(c) == 1) return c;
+        }
+
+        return '_';
+    }
+}
